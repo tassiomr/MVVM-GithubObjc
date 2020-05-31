540a058a710402fbb0722cc83471d9abb7819c36
Tassio Rocha <=>
2020-05-31 09:55:01 -0300

    Init project configuration

--- MVVM-GithubObjc/MainViewController.m
+++ MVVM-GithubObjc/MainViewController.m
@@ -6,13 +6,13 @@
 //  Copyright © 2020 Tássio Marcos Rocha. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@interface ViewController ()
+@interface MainViewController ()
 
 @end
 
-@implementation ViewController
+@implementation MainViewController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
